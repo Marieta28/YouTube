7732d1ef0d64518fe5fdf026997729aece6c33f4
Marieta28 <69346003+Marieta28@users.noreply.github.com>
2021-04-09 04:47:59 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+const swiper = new Swiper('.chanel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.chanel-button-next',
+    prevEl: '.chanel-button-prev',
+  },
+
+})
+const swiper1 = new Swiper('.chanel-slider1', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.chanel-button-next-1',
+    prevEl: '.chanel-button-prev-1',
+  },
+
+})
+
+const swiper2 = new Swiper('.chanel-slider2', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.chanel-button-next-2',
+    prevEl: '.chanel-button-prev-2',
+  },
+
+});
+
